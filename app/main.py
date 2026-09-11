98253829729ecae87d4c71907d72a6f5817270ef
davisweredev <davisweredev@gmail.com>
2026-09-11 10:20:58 +0300

    uncomment

--- app/main.py
+++ app/main.py
@@ -4,13 +4,13 @@ from app.src.database.db import db
 from app.src.models.user import User
 
 
-# def main(page: ft.Page):
-#     page.title = "Admin Dashboard"
-#     page.scroll = 'auto'
-#     db.create_tables()
-#     users_panel = UserManagementView(page)
-#     page.add(users_panel)
-#     users_panel.refresh_users()
+def main(page: ft.Page):
+    page.title = "Admin Dashboard"
+    page.scroll = 'auto'
+    db.create_tables()
+    users_panel = UserManagementView(page)
+    page.add(users_panel)
+    users_panel.refresh_users()
 
 
 ft.app(target=main)
