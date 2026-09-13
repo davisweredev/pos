9fca0daff4e7ec03ff2db9b04845b93e9e4c69a2
davisweredev <davisweredev@gmail.com>
2026-09-13 10:50:14 +0300

    switch app entrypoint to PosApp router

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
 import flet as ft
-from app.src.ui.admin.users_views import UserManagementView
-from app.src.database.db import db
-from app.src.models.user import User
+
+from app.src.ui.app import PosApp
 
 
 def main(page: ft.Page):
-    page.title = "Admin Dashboard"
-    page.scroll = 'auto'
-    db.create_tables()
-    users_panel = UserManagementView(page)
-    page.add(users_panel)
-    users_panel.refresh_users()
+    app = PosApp(page)
+    app.start()
 
 
-ft.app(target=main)
+ft.app(target=main, assets_dir="app/assets")
